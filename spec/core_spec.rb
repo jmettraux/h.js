e0d6d46656f4f2b9ffefb844e2ccd51143feaa27
John Mettraux <jmettraux@gmail.com>
2015-11-18 14:16:19 +0900

    move driver setup to spec_helpers.rb

--- spec/core_spec.rb
+++ spec/core_spec.rb
@@ -10,26 +10,13 @@ require 'spec_helpers.rb'
 
 describe 'h.js / H' do
 
-  before :all do
-
-    @driver = Selenium::WebDriver.for :phantomjs
-    #@driver = Selenium::WebDriver.for :chrome
-
-    @driver.navigate.to('file://' + File.absolute_path('spec/test.html'))
-  end
-
-  after :all do
-
-    @driver.quit
-  end
-
   describe '.elt' do
 
     it 'works .elt(selector)' do
 
-      expect(@driver.execute_script(%{
+      expect(run(%{
         return H.elt('.car.bentley .blue').textContent;
-      }).strip).to eq(
+      })).to eq(
         'blue bentley'
       )
     end
@@ -39,7 +26,7 @@ describe 'h.js / H' do
 
     it 'works  .elts(selector)' do
 
-      expect(@driver.execute_script(%{
+      expect(run(%{
         return (typeof H.elt('.car .blue'));
         return (typeof H.elts('.car .blue'));
         return H.elts('.car .blue').map(function(e) { e.textContent.trim(); });
--- spec/spec_helpers.rb
+++ spec/spec_helpers.rb
@@ -7,3 +7,20 @@
 
 require 'selenium-webdriver'
 
+
+def run(s)
+
+  $driver ||=
+    begin
+      d = Selenium::WebDriver.for :phantomjs
+      d.navigate.to('file://' + File.absolute_path('spec/test.html'))
+      d
+    end
+
+  r = $driver.execute_script(s)
+
+  r = r.strip if r.is_a?(String)
+
+  r
+end
+

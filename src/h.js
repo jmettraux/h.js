d696b2f027de79423436a1d5221a8044d6746442
John Mettraux <jmettraux@gmail.com>
2017-01-29 08:58:12 +0900

    Shave 5 more chars from H.grow()

--- src/h.js
+++ src/h.js
@@ -694,7 +694,7 @@ var H = (function() {
       'var mg=' + makeGrower.toString() + ';' +
       '"a abbr address area article aside audio b base bdi bdo blockquote br button canvas caption cite code col colgroup datalist dd del details dfn dialog div dl dt em embed fieldset figcaption figure footer form h1 h2 h3 h4 h5 h6 header hr i iframe img input ins kbd keygen label legend li main map mark menu menuitem meta meter nav noscript object ol optgroup option output p param picture pre progress q rp rt ruby s samp script section select small source span strong style sub summary sup table tbody td textarea tfoot th thead time title tr track u ul var video wbr"' +
       '.split(" ").forEach(function(n){window[n]=mg(n);});' +
-      'var __out__=(' + f.toString() + ').call();';
+      'var __out__=(' + f.toString() + ')();';
 
     var i = H.create('iframe', { style: 'width: 0; height: 0' });
     document.body.appendChild(i);
